7674d07c88c9b66371fdbf3acf2904cbf2e1645f
Dominic <dominiclupo5318@gmail.com>
2024-09-28 17:55:09 -0700

    Parse multiple simple stats from sts json file

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,44 @@ import parseJson, { JSONError } from "parse-json";
 
 type StsFile = {
   character: Character;
+  goldPerFloor: number[];
+  floorReached: number;
+  // TODO: items_purged
+  playtimeSeconds: number;
+  isAscensionMode: boolean;
+  // TODO: campfire_choices
+  // TODO: neow_cost
+  // TODO: master_deck
+  // TODO: relics
+  floorsPotionsUsed: number[];
+  // TODO: damage_taken
+  // TODO: potions_obtained
+  floorPaths: FloorPath[];
+  // TODO: items_purchased
+  campfireRests: number;
+  floorsItemsPurchased: number[];
+  floorsHp: number[];
+  // TODO: gold is this start or end gold?
+  // TODO: neow_bonus
+  isDaily: boolean;
+  isSeeded: boolean;
+  campfireUpgrades: number;
+  // TODO: win_rate How does this differ than victor field?
+  // TODO: timestamp  is this epoch start or end time of run
+  // TODO: build_version Not sure what this represents. It could be the last game change update e.g. card change.
+  // or it could be the last time this file format changed.
+  purgesPurchased: number;
+  won: boolean;
+  floorsMaxHp: number[];
+  // TODO: card_choices
+  // TODO: relics_obtained
+  // TODO: event_choices
+  // TODO: boss_relics
+  floorsItemPurged: number[];
+  isEndless: boolean;
+  floorsPotionsSpawned: number[];
+  // TODO: killed_by
+  ascensionLevel: number;
 };
 
 enum Character {
@@ -11,6 +49,17 @@ enum Character {
   Defect,
   Watcher,
   Error,
+}
+
+enum FloorPath {
+  Boss,
+  BossReward,
+  Elite,
+  Event,
+  Monster,
+  RestSite,
+  Shop,
+  Treasure,
 }
 
 const fileInput: HTMLElement = document.getElementById("data")!;
@@ -54,7 +103,7 @@ function parseFiles(files: FileList) {
           // skip ArrayBuffers
         }
       }
-      graph(stsFiles);
+      graph(filter(stsFiles));
     })
     .catch((e) => {
       console.log("error");
@@ -76,12 +125,61 @@ function parseFile(file: string): StsFile {
     character = Character.Watcher;
   } else {
     character = Character.Error;
-    console.log("unexpected character chosen");
+    console.log("unexpected character chosen: ", characterChosen);
+  }
+
+  let floorPaths: FloorPath[] = [];
+  let pathPerFloor = json.path_per_floor;
+  for (let i = 0; i < pathPerFloor.length; i++) {
+    let path = pathPerFloor[i];
+    if (path === null) {
+      floorPaths.push(FloorPath.BossReward);
+    } else if (path === "B") {
+      floorPaths.push(FloorPath.Boss);
+    } else if (path === "M") {
+      floorPaths.push(FloorPath.Monster);
+    } else if (path === "E") {
+      floorPaths.push(FloorPath.Elite);
+    } else if (path === "?") {
+      floorPaths.push(FloorPath.Event);
+    } else if (path === "R") {
+      floorPaths.push(FloorPath.RestSite);
+    } else if (path === "$") {
+      floorPaths.push(FloorPath.Shop);
+    } else if (path === "T") {
+      floorPaths.push(FloorPath.Treasure);
+    } else {
+      console.log("unexpected floor path: ", path);
+    }
   }
 
   return {
-    character: character,
+    character,
+    goldPerFloor: json.gold_per_floor,
+    floorReached: json.floor_reached,
+    playtimeSeconds: json.playtime,
+    isAscensionMode: json.is_ascension_mode,
+    floorsPotionsUsed: json.potions_floor_usage,
+    floorPaths,
+    campfireRests: json.campfire_rested,
+    floorsItemsPurchased: json.item_purchase_floors,
+    floorsHp: json.current_hp_per_floor,
+    isDaily: json.is_daily,
+    isSeeded: json.chose_seed,
+    campfireUpgrades: json.campfire_upgraded,
+    purgesPurchased: json.purchased_purges,
+    won: json.victory,
+    floorsMaxHp: json.max_hp_per_floor,
+    floorsItemPurged: json.items_purged_floors,
+    isEndless: false,
+    floorsPotionsSpawned: json.potions_floor_spawned,
+    ascensionLevel: json.ascension_level,
   };
+}
+
+function filter(files: StsFile[]): StsFile[] {
+  // TODO: filter daily, seeded, endless, etc
+  return files;
 }
 
 async function graph(files: StsFile[]) {
